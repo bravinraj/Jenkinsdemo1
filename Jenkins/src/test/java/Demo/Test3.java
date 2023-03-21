09acf5cde3cd4dca230f6bda1d707e590f9f081d
10155 <10155@DESKTOP-V23L3TE>
2023-03-21 17:57:17 +0530

    Kcube1

---
+++ Jenkins/src/test/java/Demo/Test3.java
@@ -0,0 +1,13 @@
+package Demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test3 {
+	@Test
+	public void tc3()
+	{
+		Reporter.log("hello selinium", true);
+	}
+
+}
